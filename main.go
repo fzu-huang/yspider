d21ef5912cc377e7721d10ef99f0df88d91b3f71
huang503582241 <503582241@qq.com>
2015-06-12 17:44:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	//	"sync/atomic"
 	"time"
 )
 
@@ -24,7 +25,7 @@ var Crawtcount int
 var Rec Record
 var curl chan CheckNode
 var exit chan bool
-var Count, ValidCount, ReuseCount, ImgCount int64
+var Count, ValidCount, ReuseCount, ImgCount, netErrCount, writingCount int64
 
 const (
 	Depth = 5
@@ -32,14 +33,15 @@ const (
 
 func main() {
 	Crawtcount = 0
-	leng := 30
-	curl = make(chan CheckNode, leng)
+	leng := 20
+	curl = make(chan CheckNode, 200000)
 	exit = make(chan bool, leng)
-	//m = sync.WaitGroup{}
 	Count = 0
 	ValidCount = 0
 	ReuseCount = 0
 	ImgCount = 0
+	netErrCount = 0
+	writingCount = 0
 	Rec = Record{
 		Visited: make(map[string]bool),
 		Loc:     new(sync.RWMutex),
@@ -50,7 +52,7 @@ func main() {
 
 	//url := "http://alpha.wallhaven.cc/random"
 	//url := "http://www.xinhuanet.com"
-	url := "http://tieba.baidu.com"
+	url := "http://www.baidu.com/baidu?word=福大+李妍"
 	curl <- CheckNode{url, 1}
 	for i := 0; i < leng; i++ {
 		//m.Add(1)
@@ -58,7 +60,7 @@ func main() {
 	}
 
 	//go func() {
-	ticker := time.NewTicker(time.Second * 5)
+	ticker := time.NewTicker(time.Second * 4)
 	idleticker := time.NewTicker(time.Second * 20)
 	defer idleticker.Stop()
 	defer ticker.Stop()
@@ -67,7 +69,7 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			fmt.Println("RUNNING ROUTINES COUNT: ", runtime.NumGoroutine(), ", ReVis Count:", ReuseCount, ", ImgCount:", ImgCount, ", All Visited Count: ", Count, ", Valid url Count:", ValidCount)
+			fmt.Println("RUNNING ROUTINES COUNT: ", runtime.NumGoroutine(), ", ReVis Count:", ReuseCount, ", ImgCount:", ImgCount, ", All Visited Count: ", Count, ", Valid url Count:", ValidCount, "netErrCount:", netErrCount, ", chancount", len(curl))
 			runtime.GC()
 			//fmt.Println(runtime.MemProfileRate)
 		case <-exit:
@@ -79,7 +81,11 @@ func main() {
 			if tmpvisitcount < Count {
 				tmpvisitcount = Count
 			} else {
-				fmt.Println("long time no visit url,  maybe it is stuck... mission end..")
+				fmt.Println("long time no visit url,  maybe it is stuck...waitting for file writing finish...")
+				//				if atomic.LoadInt64(&writingCount) > 0 {
+				//					continue
+				//				}
+				//				fmt.Println("write file finish . exit!")
 				return
 			}
 		}
